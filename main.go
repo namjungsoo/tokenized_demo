a49067ba1e116500673369379a5e7c5166a79ecc
Jungsoo Nam <namjungsoo@onestorecorp.com>
2019-05-31 15:27:58 +0900

    s1 == test.tokenized 필터링 완료

--- main.go
+++ main.go
@@ -15,6 +15,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"reflect"
+	"strings"
 )
 
 func main() {
@@ -42,9 +43,14 @@ func main() {
 	req.Header.Set("key", "1Azd2MmsCNpQn6jX1uwxDdUjm28VdNPD3F")
 	res, err := client.Do(req)
 
-	fmt.Println(err)
-	fmt.Println(res.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	//fmt.Println(res.Body)
 
+	/*
+		res.Body를 ReadAll 해야함
+	*/
 	bytes, _ := ioutil.ReadAll(res.Body)
 	//str := string(bytes) //바이트를 문자열로
 	//fmt.Println(str)
@@ -52,19 +58,28 @@ func main() {
 	// key: string
 	// value: []interface{}
 	var dat map[string][]interface{}
+	fmt.Println(reflect.TypeOf(dat))
 	if err := json.Unmarshal(bytes, &dat); err != nil {
 		panic(err)
 	}
-	//fmt.Println(dat)
-
-	fmt.Println(" \n \n")
 	cslice := dat["c"]
 
-	fmt.Println(reflect.TypeOf(cslice))
-
 	for i, tx := range cslice {
-		fmt.Println(i)
-		fmt.Println(tx)
-	}
+		/*
+			항상 type casting 해야 함
+		*/
+		tx2 := tx.(map[string]interface{})
+		out := tx2["out"].([]interface{})
 
+		for j, out2 := range out {
+			out3 := out2.(map[string]interface{})
+
+			if out3["s1"] != nil && strings.Compare(out3["s1"].(string), "test.tokenized") == 0 {
+				fmt.Println("i=", i)
+				fmt.Println("j=", j)
+				fmt.Println("test")
+				fmt.Println(out3)
+			}
+		}
+	}
 }
